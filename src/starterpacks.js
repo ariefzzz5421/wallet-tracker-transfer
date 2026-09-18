d31067b23f3eacf4b1b2570b735d1f984ad375a7
ariefz <ariefzzz2580@gmail.com>
2026-09-18 13:06:28 +0700

    Add supplied Notion starter pack links

---
+++ src/starterpacks.js
@@ -0,0 +1,7 @@
+export const starterPacks = [
+  { id: 'notion-01', title: 'Notion Starter 01', url: 'https://uttermost-pumpkin-971.notion.site/52c36ac9a1074b8897f7e413caa0597d?v=23118e91dbb54d6f890090ac21fe5c42' },
+  { id: 'notion-02', title: 'Notion Starter 02', url: 'https://uttermost-pumpkin-971.notion.site/9a9f8e3b939c4005a90b86e065838b2b?v=3cf2390761ca81fb9e99000c2db58f17&pvs=74' },
+  { id: 'notion-03', title: 'Notion Starter 03', url: 'https://uttermost-pumpkin-971.notion.site/b8ca8c3741d74d709e6c8e17c254818d?v=3c02390761ca816cb594000c92f2f6ce&pvs=74' },
+  { id: 'notion-04', title: 'Notion Starter 04', url: 'https://uttermost-pumpkin-971.notion.site/80f2d75809244415bae1ff4b48d78cf1?v=5fa6abd7ba704cd6b67714325200408a' },
+  { id: 'notion-05', title: 'Notion Starter 05', url: 'https://uttermost-pumpkin-971.notion.site/d0afecae9b1445fc8f73f1b44b43b75c?v=cd6f8615c95f4d9881688c70c0d23053' },
+];
